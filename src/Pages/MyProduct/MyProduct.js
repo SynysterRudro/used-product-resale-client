dd70ce7e9e08004bfd9e75d2d2ffb585bfed8d9d
SynysterRudro <rudrosingh82@gmail.com>
2022-11-26 00:43:45 +0600

    y product page completed

--- src/Pages/MyProduct/MyProduct.js
+++ src/Pages/MyProduct/MyProduct.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Contexts/AuthProvider';
+import SingleProduct from './SingleProduct';
 
 const MyProduct = () => {
+    const { user } = useContext(AuthContext);
+
+
+    const { data: products, isLoading } = useQuery({
+        queryKey: ['productCategory'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/myproducts?email=${user?.email}`)
+            const data = await res.json();
+            return data;
+        }
+    })
+
+
     return (
         <div>
-
+            {
+                products &&
+                products.map(product => <SingleProduct
+                    key={product._id}
+                    product={product}
+                ></SingleProduct>)
+            }
         </div>
     );
 };
---
+++ src/Pages/MyProduct/SingleProduct.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const SingleProduct = ({ product }) => {
+    const { product_name, img, resale_price, condition } = product;
+    return (
+        <div>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>Status</th>
+                            <th>Product name</th>
+                            <th>Product Image</th>
+                            <th>Price</th>
+                            <th>Condition</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        <tr>
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div className="avatar">
+                                        <p>Unsold</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                {product_name}
+                            </td>
+                            <td>
+                                <div className="avatar">
+                                    <div className="w-24 rounded-full">
+                                        <img src={img} alt="imageNotGiven" />
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td>
+                                {resale_price}
+                            </td>
+                            <td>
+                                {condition}
+                            </td>
+
+                        </tr>
+
+                    </tbody>
+
+                </table>
+                <hr />
+            </div>
+        </div>
+    );
+};
+
+export default SingleProduct;
